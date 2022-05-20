f5ce54c628be1ce7638cfab96ef7296ca2cc101c
Petros Amiridis <petros@amiridis.net>
2022-05-20 18:18:35 +0300

    Format code

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,8 +1,7 @@
 defmodule Dictionary do
-
   @word_list "assets/words.txt"
-  |> File.read!()
-  |> String.split(~r/\n/, trim: true)
+             |> File.read!()
+             |> String.split(~r/\n/, trim: true)
 
   def random_word do
     @word_list
