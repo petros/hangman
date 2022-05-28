46a0736fa3ed0c6c5a8027ad2f12e1aa94821ee8
petros <petros@amiridis.net>
2022-05-28 21:15:39 +0300

    Add specs

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -13,10 +13,12 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
 
+  @spec new_game() :: t()
   def new_game() do
     new_game(Dictionary.random_word())
   end
 
+  @spec new_game(String.t()) :: t()
   def new_game(word) do
     %__MODULE__{letters: word |> String.codepoints()}
   end
