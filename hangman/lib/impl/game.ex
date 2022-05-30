8f9e32ae5b72a09a5aac9721aec2f58c7c517923
petros <petros@amiridis.net>
2022-05-30 10:12:27 +0300

    Handle winning and losing games

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -13,6 +13,8 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
 
+  # new_game
+
   @spec new_game() :: t()
   def new_game() do
     new_game(Dictionary.random_word())
@@ -22,6 +24,8 @@ defmodule Hangman.Impl.Game do
   def new_game(word) do
     %__MODULE__{letters: word |> String.codepoints()}
   end
+
+  # make_move
 
   @spec make_move(t(), String.t()) :: {t(), Type.tally()}
   def make_move(game = %{game_state: state}, _guess)
@@ -36,6 +40,8 @@ defmodule Hangman.Impl.Game do
     |> return_with_tally()
   end
 
+  # accept_guess
+
   defp accept_guess(game, _guess, _already_used = true) do
     %{game | game_state: :already_used}
   end
@@ -45,20 +51,26 @@ defmodule Hangman.Impl.Game do
     |> score_guess(Enum.member?(game.letters, guess))
   end
 
+  # score_guess
+
   defp score_guess(game, _good_guess = true) do
     new_state = maybe_won(MapSet.subset?(MapSet.new(game.letters), game.used))
     %{game | game_state: new_state}
   end
 
+  defp score_guess(game = %{turns_left: 1}, _bad_guess) do
+    %{game | game_state: :lost, turns_left: 0}
+  end
+
   defp score_guess(game, _bad_guess) do
-    game
+    %{game | game_state: :bad_guess, turns_left: game.turns_left - 1}
   end
 
   defp tally(game) do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: [],
+      letters: return_guessed_letters(game),
       used: game.used |> MapSet.to_list() |> Enum.sort()
     }
   end
@@ -67,6 +79,14 @@ defmodule Hangman.Impl.Game do
     {game, tally(game)}
   end
 
+  defp return_guessed_letters(game) do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter) |> maybe_reveal(letter) end)
+  end
+
   defp maybe_won(true), do: :won
   defp maybe_won(_), do: :good_guess
+
+  defp maybe_reveal(true, letter), do: letter
+  defp maybe_reveal(_, _letter), do: "_"
 end
