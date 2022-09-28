d793b74e998201bd5c1d64661baac61e2fae603f
Petros Amiridis <petros@amiridis.net>
2022-09-28 19:08:37 +0300

    Move module attribute to a function

--- dictionary/lib/impl/world_list.ex
+++ dictionary/lib/impl/world_list.ex
@@ -1,7 +1,9 @@
 defmodule Dictionary.Impl.WordList do
-  @word_list "assets/words.txt"
-             |> File.read!()
-             |> String.split(~r/\n/, trim: true)
+  def word_list() do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/, trim: true)
+  end
 
   def random_word do
     @word_list
