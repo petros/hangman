9ac2ac9bc59ee2f32bc8a3c0389d95f593d1020f
Petros Amiridis <petros@amiridis.net>
2022-09-29 10:55:02 +0300

    Fix Dialyzer complaints

--- dictionary/lib/impl/world_list.ex
+++ dictionary/lib/impl/world_list.ex
@@ -1,5 +1,5 @@
 defmodule Dictionary.Impl.WordList do
-  @type t :: list(String)
+  @type t :: list(String.t())
 
   @spec word_list() :: t()
   def word_list() do
@@ -8,7 +8,7 @@ defmodule Dictionary.Impl.WordList do
     |> String.split(~r/\n/, trim: true)
   end
 
-  @spec random_word(t) :: String.t()
+  @spec random_word(t()) :: String.t()
   def random_word(word_list) do
     word_list
     |> Enum.random()
