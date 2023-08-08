583114e1371e19830c362d93a51f72775061c7b1
petros <petros@amiridis.net>
2023-08-09 00:55:58 +0300

    Introduce runtime server

---
+++ dictionary/lib/runtime/server.ex
@@ -0,0 +1,11 @@
+defmodule Dictionary.Runtime.Server do
+  alias Dictionary.Impl.WordList
+
+  def start_link do
+    Agent.start_link(&WordList.word_list/0)
+  end
+
+  def random_word(pid) do
+    Agent.get(pid, &WordList.random_word/1)
+  end
+end
