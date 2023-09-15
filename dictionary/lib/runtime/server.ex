0733a3fa9fd728497853bfde9a5083ec408d66af
Petros Amoiridis <petros@hey.com>
2023-09-15 20:24:53 +0300

    Make backend use the running application

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -2,10 +2,10 @@ defmodule Dictionary.Runtime.Server do
   alias Dictionary.Impl.WordList
 
   def start_link do
-    Agent.start_link(&WordList.word_list/0)
+    Agent.start_link(&WordList.word_list/0, name: :wilma)
   end
 
-  def random_word(pid) do
-    Agent.get(pid, &WordList.random_word/1)
+  def random_word() do
+    Agent.get(:wilma, &WordList.random_word/1)
   end
 end
